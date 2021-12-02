670af1e3a80deecd5ec8f7e7af12a2781d4e6f27
Justin747 <justin747@gmail.com>
2021-12-01 20:58:21 -0800

    Created base Tab Bar
    Created base Tab Bar with UITabBarController

---
+++ GitHub Followers/FavoritesListVC.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesListVC.swift
+//  GitHub Followers
+//
+//  Created by Justin747 on 12/1/21.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GitHub Followers/SearchVC.swift
@@ -0,0 +1,30 @@
+//
+//  SearchVC.swift
+//  GitHub Followers
+//
+//  Created by Justin747 on 12/1/21.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
