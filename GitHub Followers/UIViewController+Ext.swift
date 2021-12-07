1511be6ebec7eba457e9c0cad7ffa360da723300
Justin747 <justin747@gmail.com>
2021-12-06 21:09:29 -0800

    Custom Alert Complete
    Added customizable button, button title, and button message for custom alert
    
    Created an extension for mutability and reusability

---
+++ GitHub Followers/UIViewController+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Ext.swift
+//  GitHub Followers
+//
+//  Created by Justin747 on 12/6/21.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
