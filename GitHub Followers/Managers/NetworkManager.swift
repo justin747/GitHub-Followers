918591e0e1167cb886042bbb945e061e5bbda833
Justin747 <justin747@gmail.com>
2021-12-09 20:52:34 -0800

    Created NetworkManager
    Created NetworkManager to call api, process data from API, and throw errors if there is any issue with the API call

---
+++ GitHub Followers/Managers/NetworkManager.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkManager.swift
+//  GitHub Followers
+//
+//  Created by Justin747 on 12/9/21.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    let baseUrl = "https://api.github.com"
+    
+    private init() {}
+        
+        func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+            let endpoint = baseUrl + "/users/\(username)/followers?per_page100&page=\(page)"
+            
+            guard let url = URL(string: endpoint) else {
+                completed(nil, "This username created invalid request. Please try again")
+                return
+            }
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                
+                if let _ = error {
+                    completed(nil, "Unable to complete request. Please check your network connection")
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    completed(nil, "Invalied response from the server. Please try again.")
+                    return
+                }
+                
+                guard let data = data else {
+                    completed(nil, "The data received from the server is invalid. Please try again")
+                    return
+                }
+                
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    let followers = try decoder.decode([Follower].self, from: data)
+                    completed(followers, nil)
+                } catch {
+                    completed(nil, "The data received from the server is invalid. Please try again")
+                }
+            }
+            
+            task.resume()
+    }
+}
