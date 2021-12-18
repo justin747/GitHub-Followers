7dcc28f543d5a451dcdef82dd6d5e0fffdc256bd
Justin747 <justin747@gmail.com>
2021-12-17 20:38:26 -0800

    Refactored Three Column Flow Layout
    Refactored Three Column Flow Layout function into separate swift file to clean up FollowerListVC

--- GitHub Followers/Screens/FollowerListVC.swift
+++ GitHub Followers/Screens/FollowerListVC.swift
@@ -42,39 +42,28 @@ class FollowerListVC: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemTeal
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
-        
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return UICollectionViewFlowLayout()
-    }
+   
     
     func getFollowers() {
         
         NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
             
+            guard let self = self else { return }
+            
             switch result {
                 
             case .success(let followers):
-                self?.followers = followers
-                self?.updateData()
+                self.followers = followers
+                self.updateData()
                 
             case .failure(let error):
-                self?.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "OK")
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "OK")
                 
             }
         }
