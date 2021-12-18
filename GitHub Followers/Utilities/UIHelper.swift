68fcddbb1cc3353af9785c17cafd155e36a9c416
Justin747 <justin747@gmail.com>
2021-12-17 21:07:45 -0800

    Updated OS target and fixed UI bug
    Updated OS target to iOS 15 for testing, and fixed UI bug in the Follower List caused by iOS 15
    
    Follower List now properly shown in 3 column grid

--- GitHub Followers/Utilities/UIHelper.swift
+++ GitHub Followers/Utilities/UIHelper.swift
@@ -21,6 +21,6 @@ struct UIHelper {
         flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
         flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
         
-        return UICollectionViewFlowLayout()
+        return flowLayout
     }
 }
