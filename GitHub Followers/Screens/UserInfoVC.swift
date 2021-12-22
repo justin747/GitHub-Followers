95137933833c3f9fe93c04156c536f6ea0145ad3
Justin747 <justin747@gmail.com>
2021-12-22 00:24:26 -0800

    Added Modal sheet to Follower List
    Added Modal sheet to Follower List to display selected follower's info when avatar is clicked

---
+++ GitHub Followers/Screens/UserInfoVC.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfoVC.swift
+//  GitHub Followers
+//
+//  Created by Justin747 on 12/21/21.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
