60732a2f69a0bc0d5d527471e2031804372d462e
Justin747 <justin747@gmail.com>
2021-12-24 20:32:27 -0800

    Built HeaderView
    Built HeaderView to display a user's name, username, location, and bio

---
+++ GitHub Followers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHub Followers
+//
+//  Created by Justin747 on 12/24/21.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
