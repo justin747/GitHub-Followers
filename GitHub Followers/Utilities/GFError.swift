994ac046d3fe3412a080a0363be3d9642d0c2cdb
Justin747 <justin747@gmail.com>
2021-12-27 21:53:22 -0800

    Added Persistence Logic for Favorites
    Created @escaping functions to add and remove Favorite Users from Favorites List

--- GitHub Followers/Utilities/GFError.swift
+++ GitHub Followers/Utilities/GFError.swift
@@ -9,9 +9,11 @@ import Foundation
 
 enum GFError: String, Error {
     
-    case invalidUsername = "This username created invalid request. Please try again"
-    case unableToComplete = "Unable to complete request. Please check your network connection"
+    case invalidUsername = "This username created invalid request. Please try again."
+    case unableToComplete = "Unable to complete request. Please check your network connection."
     case invalidResponse = "Invalid response from the server. Please try again."
-    case invalidData = "The data received from the server is invalid. Please try again"
+    case invalidData = "The data received from the server is invalid. Please try again."
+    case unableTofavorite = "There was an error adding user to Favorites. Please try again."
+    case alreadyInFavorites = "You already added this user to Favorites. You must REALLY like them."
     
 }
