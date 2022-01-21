95bc1a2b099a523d18b43a046e3339ff81c8a221
Justin747 <justin747@gmail.com>
2022-01-20 17:47:53 -0800

    Multiple updates
    * Moved initializers from UserInfoVC into GFFollowerItemVC and GFRepoItemVC
    
    * updated favorites logic in persistence manager
    
    * created extension to remove excess cells on short favorite lists
    
    * combined UI update with persistence manager call when favorites are deleted

---
+++ GitHub Followers/Extensions/UITableVIew+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UITableVIew+Ext.swift
+//  GitHub Followers
+//
+//  Created by Justin747 on 1/19/22.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async {
+            self.reloadData()
+        }
+    }
+    
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero)
+    }
+}
