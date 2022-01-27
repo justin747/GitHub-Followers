3ae70e4fc1d47a54950362909145ade5fefb5808
Justin747 <justin747@gmail.com>
2022-01-27 12:14:11 -0800

    Changed color Get Followers button
    Minor update. Changing colors of all buttons

--- GitHub Followers/Custom Views/ViewControllers/Item Info VCs/GFFollowerItemVC.swift
+++ GitHub Followers/Custom Views/ViewControllers/Item Info VCs/GFFollowerItemVC.swift
@@ -32,7 +32,7 @@ class GFFollowerItemVC: GFItemInfoVC {
     private func configureItems() {
         itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
         itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
-        actionButton.set(color: .systemIndigo, title: "Get Followers", systemImageName: "person.3")
+        actionButton.set(color: .systemTeal, title: "Get Followers", systemImageName: "person.3")
     }
     
     override func actionButtonTapped() {
